d0caa0ab38f8c85e6d6bfb510fd903f2b120265c
Felipe Marques de Araujo <felipe_marques_araujo@hotmail.com>
2022-08-15 17:55:14 -0300

    Adicionando endpoints de consulta por ID e de criação

--- src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -3,6 +3,7 @@ package one.digitalinnovation.parking.service;
 import one.digitalinnovation.parking.model.Parking;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,8 +16,11 @@ public class ParkingService {
 
     static {
         var id = getUUID();
+        var id1 = getUUID();
         Parking parking = new Parking(id,"DMS-1113","SC","CELTA","PRETO");
+        Parking parking1 = new Parking(id1,"DXJ-1113","SP","ZAFIRA","PRATA");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
     }
 
     private static String getUUID(){
@@ -27,4 +31,15 @@ public class ParkingService {
         return parkingMap.values().stream().collect(Collectors.toList());
     }
 
+    public Parking findById(String id) {
+        return parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
+    }
 }
